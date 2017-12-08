0b19dd7a30e280f97e423bfa30f71149211beaa7
Kyo Yamada <hysteric.madam@gmail.com>
2017-12-08 22:14:05 +0900

    add head.title, head.meta, head.link

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  head: {
+    title: 'HOMECINEMA & AUDIO Co., Ltd.',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'favicon', href: 'favicon.ico' }
+    ]
+  },
   css: [
     'normalize.css/normalize.css'
   ]
